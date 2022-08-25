158e92786ed4867026fb30bfbabd095f1838355d
laroca <laroca73@gmail.com>
2022-08-25 19:07:04 +0200

    New Task Page + task.js new-task post

--- src/pages/NewTask.vue
+++ src/pages/NewTask.vue
@@ -1,24 +1,62 @@
 <template>
- <Navbar :pageTitle="pageTitle" :pageLink="pageLink"/>
-  <div class="newtasks__wrapper">
-
+  <Navbar :pageTitle="pageTitle" :pageLink="pageLink" />
+  <div class="newtask__wrapper">
+    <form class="newtask__form" @submit.prevent="submitTask">
+      <fieldset class="newtask__fieldset">
+        <input
+          v-model="taskTitle"
+          class="newTask__form-element"
+          type="text"
+          name="taskTitle"
+          id="taskTitle"
+          placeholder="New Task Title"
+          required
+        />
+      </fieldset>
+      <button class="newTask__form-button" type="submit">Write task</button>
+    </form>
+    <div v-if="msgErrors.length > 0" class="bbdd__messages error">
+      {{ msgErrors[0].message }} <br />
+      {{ msgErrors[0].status }}
+    </div>
   </div>
 </template>
 
 <script>
-import Navbar from '../components/Navbar.vue';
-export default {
-    components: { Navbar },
-    data() {
-        return {
-            pageTitle: "New Task",
-            pageLink:"Back",        
-        };
-    },
-}
+import Navbar from "../components/Navbar.vue";
+import { useUserStore } from "../store/user.js";
+import { useTaskStore } from "../store/task.js";
+import { useRouter } from "vue-router";
 
+export default {
+  components: { Navbar },
+  data() {
+    return {
+      pageTitle: "New Task",
+      pageLink: "Back",
+      taskTitle: "",
+      taskDescription: "",
+      msgErrors: [],
+      router: useRouter(),
+      userStore: useUserStore(),
+      taskStore: useTaskStore(),
+    };
+  },
+  methods: {
+    async submitTask() {
+      console.log("entra");
+      if (this.taskTitle) {
+        try {
+          await this.taskStore.newTask(this.taskTitle, this.userStore.user.id);
+          this.router.push({ path: "/dashboard" });
+        } catch (e) {
+          this.msgErrors.push(e);
+        }
+      }
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/store/task.js
+++ src/store/task.js
@@ -15,7 +15,12 @@ export const useTaskStore = defineStore("tasks", {
         .order("id", { ascending: false });
       this.tasks = tasks;
     },
-    // Hacer POST
+    async newTask (taskTitle, userID) {
+      await supabase
+        .from('tasks')
+        .insert([{ title: taskTitle, is_complete: false, user_id: userID }]);
+      this.fetchTasks();
+    },
     // Hacer el PUT (edit)
     // Hacer el delete
     // Hacer el PUT (cambiar entre completada y pendiente)
