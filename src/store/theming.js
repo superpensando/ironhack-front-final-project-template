a617621526814e1989483863071ef041b1e129cd
laroca <laroca73@gmail.com>
2022-09-15 20:51:47 +0200

    Create theming table in supabase to set the themings + Theming Store & read theming by user

---
+++ src/store/theming.js
@@ -0,0 +1,20 @@
+// /store/theming.js
+
+import { defineStore } from "pinia";
+import { supabase } from "../supabase";
+
+export const useThemingStore = defineStore("theming", {
+  state: () => ({
+    theming: null,
+  }),
+  actions: {
+    async fetchTheming(userID) {
+        const { data: theming } = await supabase
+        .from("theming")
+        .select("theme")
+        .match({ user_id: userID });
+      this.theming = theming;
+      //console.log(theming);
+    },
+  },
+});
